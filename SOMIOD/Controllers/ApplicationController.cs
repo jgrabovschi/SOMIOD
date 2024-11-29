0a81b8f00fbb3006eb82bd24f9eb348a40ae9df6
jgrabovschi <iorgusor@gmail.com>
2024-11-29 19:10:00 +0000

    init

---
+++ SOMIOD/Controllers/ApplicationController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MongoDB.Driver;
+using SOMIOD.Models;
+
+
+namespace SOMIOD.Controllers
+{
+    public class ApplicationController : ApiController
+    {
+        const string databaseURL = "mongodb://mongodb.cloud-ss.pt:27017";
+        // GET: api/Application
+        public IHttpActionResult Get()
+        {
+            var client = new MongoClient(databaseURL);
+            Console.Write("connected");
+            var database = client.GetDatabase("somiod");
+            var collection = database.GetCollection<Application>("applications");
+            var applications = collection.Find(_ => true).ToList();
+
+
+            return Ok(applications);
+        }
+
+        // GET: api/Application/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Application
+        public IHttpActionResult Post([FromBody]Application application)
+        {
+            var client = new MongoClient(databaseURL);
+            Console.Write("connected");
+            var database = client.GetDatabase("somiod");
+            var collection = database.GetCollection<Application>("applications");
+            var applicationToInsert = new Application
+            {
+                id = application.id,
+                name = application.name,
+                creation_datetime = DateTime.Now
+            };
+            collection.InsertOne(applicationToInsert);
+
+            return Ok();
+        }
+
+        // PUT: api/Application/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Application/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SOMIOD/Models/Application.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOMIOD.Models
+{
+    public class Application
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime creation_datetime { get; set; }
+
+    }
+}
