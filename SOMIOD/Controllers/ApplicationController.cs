d7075056e50a97213ba86c0ff76b029cad303dd8
jgrabovschi <iorgusor@gmail.com>
2024-12-06 15:51:52 +0000

    auth

--- SOMIOD/Controllers/ApplicationController.cs
+++ SOMIOD/Controllers/ApplicationController.cs
@@ -13,12 +13,44 @@ namespace SOMIOD.Controllers
     public class ApplicationController : ApiController
     {
         const string databaseURL = "mongodb://mongodb.cloud-ss.pt:27017";
-        // GET http://<domain:port>/api/somiod/
+
+
+        private MongoClient Authenticate()
+        {
+            MongoCredential credential = MongoCredential.CreateCredential("admin", "somiod_user", ",$0m1O3");
+            MongoClientSettings settings = MongoClientSettings.FromConnectionString(databaseURL);
+
+            settings.Credential = credential;
+
+            MongoClient mongoClient = null;
+
+            try
+            {
+                mongoClient = new MongoClient(settings);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            return mongoClient;
+
+        }
+
+
+        // GET: api/Application
         public IHttpActionResult Get()
         {
-            var client = new MongoClient(databaseURL);
+            var mongoClient = Authenticate();
+            if (mongoClient == null)
+            {
+                // Assuming the password was right, the only reason for this to fail is if the connection to the database fails
+                return StatusCode(HttpStatusCode.InternalServerError);
+            }
+
             Console.Write("connected");
-            var database = client.GetDatabase("somiod");
+            var database = mongoClient.GetDatabase("somiod");
             var collection = database.GetCollection<Application>("applications");
             var applications = collection.Find(_ => true).ToList();
 
@@ -35,9 +67,14 @@ namespace SOMIOD.Controllers
         // POST: api/somioid/
         public IHttpActionResult Post([FromBody]Application application)
         {
-            var client = new MongoClient(databaseURL);
+            var mongoClient = Authenticate();
+            if (mongoClient == null)
+            {
+                // Assuming the password was right, the only reason for this to fail is if the connection to the database fails
+                return StatusCode(HttpStatusCode.InternalServerError);
+            }
             Console.Write("connected");
-            var database = client.GetDatabase("somiod");
+            var database = mongoClient.GetDatabase("somiod");
             var collection = database.GetCollection<Application>("applications");
             var applicationToInsert = new Application
             {
@@ -45,7 +82,15 @@ namespace SOMIOD.Controllers
                 name = application.name,
                 creation_datetime = DateTime.Now
             };
-            collection.InsertOne(applicationToInsert);
+
+            try
+            {
+                collection.InsertOne(applicationToInsert);
+            }
+            catch (Exception)
+            {
+                return StatusCode(HttpStatusCode.BadRequest);
+            }
 
             return Ok();
         }
